cccbcb4272dd7bcb802784f531b1674b87129b1f
SahilBachu <sahilbachu03@gmail.com>
2025-11-22 14:44:46 -0500

    Frontend: created header with logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,11 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import Header from "./components/Header";
 
 function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    axios
-      .get("http://localhost:5000/msg")
-      .then((response) => setMessage(response.data.message))
-      .catch((error) => console.error("Error: ", error));
-  }, []);
-
   return (
     <div>
-      <h1>{message}</h1>
+      <Header />
     </div>
   );
 }

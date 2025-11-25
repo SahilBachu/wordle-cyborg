353c381a868af12d39c861121fc19f02f8dfcd4e
SahilBachu <sahilbachu03@gmail.com>
2025-11-24 19:21:18 -0500

    fix: FileNotFound bug

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import solver
 
 app = Flask(__name__)
